dc881ab7eddc558579a6d5957ddb4dd220270a84
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-25 14:58:44 +0300

    define a test  with methods that start with test_ and assert a  TestCase method (self.assertEqual()) that checks for an expected result

--- user_test.py
+++ user_test.py
@@ -1,15 +1,14 @@
 import unittest  # Importing the unittest module
 from user import User  # Importing the contact class
-
- class TestUser  (unittest.TestCase): # create a subclass class called TestUser, that inherits from unittest.TestCase
+class TestUser  (unittest.TestCase): # create a subclass class called TestUser, that inherits from unittest TestCase
     def setUp(self):
         self.new_user = User("feysal", "ibrahim", "123456789")  # create user object
         User.user_list = []
 
-#     def test_init(self):
-#         self.assertEqual(self.new_user.first_name, "feysal")
-#         self.assertEqual(self.new_user.last_name, "ibrahim")
-#         self.assertEqual(self.new_user.password, "123456789")
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name, "feysal")
+        self.assertEqual(self.new_user.last_name, "ibrahim")
+        self.assertEqual(self.new_user.password, "123456789")
 #
 # if __name__ == '__main__':
 #     unittest.main()
