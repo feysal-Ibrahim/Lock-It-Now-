3ec7e1589bed2ecc027720889f1f068c4f764dbe
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-25 14:42:49 +0300

    generate __init__ method which  helps us define our object in the user test module

--- user.py
+++ user.py
@@ -2,17 +2,17 @@ class User:
     """
        Class that generates new instances of users
        """
-    # user_list = []
+    user_list = []
 
-    # Init method down here
+    Init method down here
 
-    # def __init__(self, first_name, last_name, password):
-    #     """
-    #           __init__ method helps us define our object
-    #           Args:
-    #               user_name: New user name.
-    #               password: New user password.
-    #           """
+    def __init__(self, first_name, last_name, password):
+        """
+              __init__ method helps us define our object
+              Args:
+                  user_name: New user name.
+                  password: New user password.
+              """
 
         # self.first_name = first_name
         # self.last_name = last_name
