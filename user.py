39d0776638a00ca956b814497b48a6cad8a4c764
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-25 14:38:17 +0300

    create credential and user data folders

---
+++ user.py
@@ -0,0 +1,20 @@
+class User:
+    """
+       Class that generates new instances of users
+       """
+    user_list = []
+
+    # Init method down here
+
+    # def __init__(self, first_name, last_name, password):
+    #     """
+    #           __init__ method helps us define our object
+    #           Args:
+    #               user_name: New user name.
+    #               password: New user password.
+    #           """
+
+        # self.first_name = first_name
+        # self.last_name = last_name
+        # self.password = password
+        #attribute for object
---
+++ user_test.py
@@ -0,0 +1,15 @@
+import unittest  # Importing the unittest module
+from user import User  # Importing the contact class
+
+# class TestContact  (unittest.TestCase):
+#     def setUp(self):
+#         self.new_user = User("feysal", "ibrahim", "123456789")  # create user object
+#         User.user_list = []
+#
+#     def test_init(self):
+#         self.assertEqual(self.new_user.first_name, "feysal")
+#         self.assertEqual(self.new_user.last_name, "ibrahim")
+#         self.assertEqual(self.new_user.password, "123456789")
+#
+# if __name__ == '__main__':
+#     unittest.main()
