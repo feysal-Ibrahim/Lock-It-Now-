072917d9bb30837388d57f414aaaf3c07d54d367
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-26 19:45:47 +0300

    generate new instances of account credentials

--- credentials.py
+++ credentials.py
@@ -2,15 +2,15 @@ import random
 
 
 # class Credential:
-#     '''
-#        Will generate new instances of account credentials
-#        '''
-#     credential_list:[]
-#
-#     def __init__(self, account_name, password):
-#         '''
-#         Passed in three arguments of the isntances of our variables
-#         '''
-#         self.account_name = account_name
-#         self.password = password
+     '''
+        Will generate new instances of account credentials
+        '''
+     credential_list:[]
+
+     def __init__(self, account_name, password):
+         '''
+         Passed in three arguments of the isntances of our variables
+         '''
+           self.account_name = account_name
+           self.password = password
 
