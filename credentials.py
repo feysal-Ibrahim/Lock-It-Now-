9d3d7f9133ac856a9f7700eaf3befea53839a91b
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-26 22:43:13 +0300

    create attribute for object credentials and  method to save credential

--- credentials.py
+++ credentials.py
@@ -5,7 +5,7 @@ class Credential:
      '''
         Will generate new instances of account credentials
         '''
-     credential_list:[]
+     credentials_list:[]
 
      def __init__(self, account_name, password):
           '''
@@ -13,4 +13,12 @@ class Credential:
            '''
           self.account_name = account_name
           self.password = password
+          """
+            create attribute for object.
+            """
 
+     def save_user(self):
+               Credential.credentials_list.append(self)
+               '''
+              create method to save credential
+                '''
--- credentials_test.py
+++ credentials_test.py
@@ -3,8 +3,12 @@ from credentials import Credential # Importing the credential class
 class TestCredential  (unittest.TestCase): # create a subclass  called TestUser, that inherits from unittest TestCase
     def setUp(self):
         self.new_user = Credential("facebook", "673766")  # create user object
-        Credential.credential_list = []
+        Credential.credentials_list = []
 
     def test_init(self):
-         self.assertEqual(self.new_user.first_name, "feysal")
-         self.assertEqual(self.new_user.password, "123456789")
+         self.assertEqual(self.new_credentials.account_name, "feysal")
+         self.assertEqual(self.new_credentials.password, "123456789")
+
+    def test_save_user(self):
+             self.new_credentials.save_credentials()  # saving the new credentials
+             self.assertEqual(len(Credential.credentials_list), 1)
