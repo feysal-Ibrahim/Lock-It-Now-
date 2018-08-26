bb38981a813ed9f2ac4cf994fd79ab1395291422
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-26 19:48:19 +0300

    crete attribute for class credential

--- credentials.py
+++ credentials.py
@@ -1,16 +1,16 @@
 import random
 
 
-# class Credential:
+class Credential:
      '''
         Will generate new instances of account credentials
         '''
      credential_list:[]
 
      def __init__(self, account_name, password):
-         '''
-         Passed in three arguments of the isntances of our variables
-         '''
-           self.account_name = account_name
-           self.password = password
+          '''
+           Passed in three arguments of the isntances of our variables
+           '''
+          self.account_name = account_name
+          self.password = password
 
